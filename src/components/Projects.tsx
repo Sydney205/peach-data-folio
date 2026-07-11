b01f2f8b436c14c0b8737c28ec438d60b3e827cc
Ѕуԁոεу <diekechi@gmail.com>
2026-07-11 20:17:12 +0100

    Remove DashLink project from Projects list
    Removed DashLink project details from Projects component.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -22,14 +22,6 @@ export function Projects() {
       demo: "https://dashlink-5mnl.onrender.com"
     },
     {
-      title: "DashLink",
-      description: "A simple and efficient URL shortener that transforms long URLs into compact, shareable links. Features include custom aliases and easy integration for streamlined link management.",
-      image: "/6a5253abe494773e183ce34d.png",
-      tags: ["pnpm", "vite", "Express", "short-links"],
-      github: "https://github.com/Sydney205/DashLink",
-      demo: "https://dashlink-5mnl.onrender.com"
-    },
-    {
       title: "Tico",
       description: "A simple and efficient URL shortener that transforms long URLs into compact, shareable links. Features include custom aliases and easy integration for streamlined link management.",
       image: "/tico.png",
